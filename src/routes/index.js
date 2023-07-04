727333587b4e49ec473c8945bd98e78da59fd95d
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-07-04 14:37:32 +0530

    format_applied

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,41 +1,41 @@
-import { Router } from 'express';
-import { web3, myContract, account } from '../instance.js';
-import multer from 'multer';
-import { create } from 'ipfs-http-client';
+import { Router } from "express";
+import { web3, myContract, account } from "../instance.js";
+import multer from "multer";
+import { create } from "kubo-rpc-client";
 const router = Router();
 const upload = multer();
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index');
+router.get("/", function (req, res) {
+  res.render("index");
 });
 
-router.get('/view', function (req, res) {
+router.get("/view", function (req, res) {
   let data = {
-    course: '<Course>',
-    certificateID: '<ID>',
-    name: '<Name>',
-    grade: '<Grade>',
-    date: '<Date>',
+    course: "<Course>",
+    certificateID: "<ID>",
+    name: "<Name>",
+    grade: "<Grade>",
+    date: "<Date>",
   };
-  res.render('viewCertificate', { data: data });
+  res.render("viewCertificate", { data: data });
 });
 
-router.get('/issue', function (req, res) {
-  res.render('issueCertificate', {
-    formClass: '',
-    messageClass: 'hidden',
-    certificateID: '<ID>',
+router.get("/issue", function (req, res) {
+  res.render("issueCertificate", {
+    formClass: "",
+    messageClass: "hidden",
+    certificateID: "<ID>",
   });
 });
 
-router.post('/issue', upload.single('document'), async function (req, res) {
+router.post("/issue", upload.single("document"), async function (req, res) {
   let data = req.body;
   console.log(data);
   let document = req.file;
 
   /* IPFS */
-  const ipfs = create('/ip4/127.0.0.1/tcp/5001');
+  const ipfs = create("/ip4/127.0.0.1/tcp/5001");
   const upload = await ipfs.add(document.buffer);
   console.log(upload);
   const docHash = web3.utils.asciiToHex(upload.path);
@@ -48,20 +48,20 @@ router.post('/issue', upload.single('document'), async function (req, res) {
       data.name,
       data.grade,
       data.date,
-      docHash,
+      docHash
     )
-    .send({ from: account, gasLimit: '927000' })
+    .send({ from: account, gasLimit: "927000" })
     .then((txn) => {
       console.log(txn);
-      res.render('issueCertificate', {
-        formClass: 'hidden',
-        messageClass: '',
+      res.render("issueCertificate", {
+        formClass: "hidden",
+        messageClass: "",
         certificateID: data.certificateID,
       });
     });
 });
 
-router.post('/view', function (req, res) {
+router.post("/view", function (req, res) {
   let data = req.body;
   console.log(data);
 
@@ -72,7 +72,7 @@ router.post('/view', function (req, res) {
       console.log(result);
       result.certificateID = data.certificateID;
       result.document = web3.utils.hexToAscii(result.document);
-      res.render('viewCertificate', { data: result });
+      res.render("viewCertificate", { data: result });
     });
 });
 
